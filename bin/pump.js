367ef7722bcc9e703456f798f9f2fdd76e5a5ee2
ShaunLWM <shaunlimweimin@gmail.com>
2018-07-20 21:24:52 +0800

    Update module to use publish file-pumper package

--- bin/pump.js
+++ bin/pump.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const fs = require('fs');
+const pump = require('node-file-pumper');
 
 const VERSION = require('../package').version;
 
@@ -17,29 +17,12 @@ if (!process.argv.slice(4).length) {
     return console.log('[-] Missing argument!\n[+] Usage: pumper -f [file-name] -s [size] -t [-mb/-kb]');
 }
 
-let file = `./${program.file}`;
+let file = program.file;
 let size = parseInt(program.size);
 let type = program.type;
-
-console.log(file, size, type);
-let bsize = 0;
-let fopen = fs.openSync(file, 'w');
-switch (type) {
-    case 'kb':
-        bsize = size * 10024;
-        break;
-    case 'mb':
-        bsize = size * 1048576;
-        break;
-    default:
-        return console.log("Use mb or kb");
-        break;
-}
-
-let bufferSize = 256;
-for (i = 0; i < bsize / bufferSize; i++) {
-    fs.writeSync(fopen, '0'.repeat(bufferSize));
-}
-
-fs.closeSync(fopen);
-return console.log("Done!");
+new Promise((resolve, reject) => {
+    return pump(file, size, type, (error) => {
+        if (error) { return reject(error); }
+        return resolve();
+    });
+});
